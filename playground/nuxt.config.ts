17cbcca48b11310bb383d430961c296462e46133
Gary Woodfine <lnb0l9dc@duck.com>
2025-07-26 01:07:18 +0100

    Upgrade Nuxt and related dependencies to v4.0.1 in `package.json` and `pnpm-lock.yaml`.

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,4 +1,8 @@
 export default defineNuxtConfig({
   modules: ["../src/module"],
   devtools: { enabled: false },
+  compatibilityDate: '2025-07-26',
+  fathom: {
+    siteId: process.env.fathom_analytics_id
+  },
 });
